712e6e97ff7779e07eee9d3a2fcc4c4ca628599d
franrbb <fran89rbb@hotmail.com>
2020-09-19 18:10:27 +0200

    Configuracion del aplication.properties para conectar con la base de datos

---
+++ src/test/java/com/springboot/empleos/app/SpringBootEmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.empleos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
