ea75460a5a510f98e11a226eeb6c720d6cbdd7dc
franrbb <fran89rbb@hotmail.com>
2020-10-12 23:26:28 +0200

    Añadiendo las clases entity Usuario y Perfil

---
+++ src/main/java/com/springboot/empleos/app/models/entity/Perfil.java
@@ -0,0 +1,35 @@
+package com.springboot.empleos.app.models.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "perfiles")
+public class Perfil {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String perfil;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+
+}
